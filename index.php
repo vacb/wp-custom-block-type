54258a20b11ee4d721731ca4345e9e92418d853a
Victoria Blackburn <victoria@sixtwo.tech>
2021-08-16 16:37:03 +0100

    Test to make sure JS is loading for post edit screen

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 /*
-  Plugin Name: Word Filter Plugin
-  Description: Replaces a list of words.
+  Plugin Name: Custom Block Type Plugin
+  Description: Custom Block Type
   Version: 1.0
   Author: Victoria
   Author URI: https://github.com/vacb
@@ -10,3 +10,17 @@
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+class CustomBlock {
+  function __construct() {
+    // Load block when on new block screen
+    add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+  }
+
+  function adminAssets() {
+    // Give name, location, dependencies
+    wp_enqueue_script('customblocktype', plugin_dir_url(__FILE__) . 'test.js', array('wp-blocks'));
+  }
+}
+
+$customBlock = new CustomBlock();
